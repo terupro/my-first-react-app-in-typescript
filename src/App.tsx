e4b60c49455a6d51b7cfc4fdd592e23545ef4a0e
‘terupro’ <terupro911@gmail.com>
2022-09-03 08:59:23 +0900

    function-components-in-typescript-basic is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
-interface AppProps {
-  message: string;
-}
-
-const App = ({ message }: AppProps) => {
-  return <div>{message}</div>;
-=======
-// Propsで受け取る型を指定
 interface AppProps {
   message?: string;
 }
@@ -20,7 +11,6 @@ const App: React.FC<AppProps> = ({ message }) => {
 // Propsで受け取る初期値の指定
 App.defaultProps = {
   message: "Hello, defaultProps!",
->>>>>>> @{-1}
 };
 
 export default App;
