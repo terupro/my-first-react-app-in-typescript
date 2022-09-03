dd8f0fbdd832cf3a329c8dcad3671026bad927ae
‘terupro’ <terupro911@gmail.com>
2022-09-03 09:46:48 +0900

    useRef is done

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Counter: React.FC<{}> = () => {
   const initialValue: any = 0;
@@ -9,11 +9,20 @@ const Counter: React.FC<{}> = () => {
   const increment = () => setValue((prevState) => prevState + 1);
   const decrement = () => setValue((prevState) => prevState - 1);
 
+  // DOMを参照する
+  const renderTimes = useRef<number>(0);
+
+  // レンダリング時に実行する
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
     </div>
   );
 };
